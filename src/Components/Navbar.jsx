d1e2618fe520a92f7f65bbc41b20779502fdd6a0
Raymund Ogsimer <raymund.aicpa@gmail.com>
2024-01-18 02:39:55 +0800

    Wrapper

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,9 @@
-import { Search } from '@mui/icons-material';
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
     height: 60px;
 `
-
-
-
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
@@ -18,18 +14,10 @@ const Left = styled.div`
 flex: 1;
 `;
 
-const Language = styled.span`
-    font-size: 14;
-    cursor: pointer;
-`
-
-const SearchContainer = styled.div`
-    border: 1px solid lightgray;
-`
-
 const Center = styled.div`
 flex: 1;
 `;
+
 const Right = styled.div`
 flex: 1;
 `
@@ -39,14 +27,10 @@ const Navbar = () => {
 <Container>
     <Wrapper>
         <Left>
-          <Language>EN</Language>
-          <SearchContainter>
-              input
-              <Search/>
-          </SearchContainter>
+          Left
         </Left>
-        <Center>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos perferendis ipsum amet unde sapiente provident nam voluptatum ratione quidem atque eius obcaecati vel ab ipsa repudiandae odit, quisquam dolores fugiat!</Center>
-        <Right>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi cumque debitis, perferendis illum numquam deserunt animi nihil optio aliquam a vero excepturi ipsum officiis tenetur blanditiis corrupti incidunt? Dolorem, earum?</Right>
+        <Center>center</Center>
+        <Right>right</Right>
     </Wrapper>
 </Container>
   )
