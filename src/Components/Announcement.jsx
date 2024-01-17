ac5690118f6bc728bfeed6b260416029e16fe57f
Raymund Ogsimer <raymund.aicpa@gmail.com>
2024-01-18 03:47:50 +0800

    Announcement component

--- src/Components/Announcement.jsx
+++ src/Components/Announcement.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
         height: 30px;
         background-color: teal;
+        color: white;
 `
 
 const Announcement = () => {
