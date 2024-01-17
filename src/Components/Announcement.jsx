d17d615086cee1541cd1c7b87c063f00763e2834
Raymund Ogsimer <raymund.aicpa@gmail.com>
2024-01-18 03:44:02 +0800

    Announcement

---
+++ src/Components/Announcement.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+        height: 30px;
+        background-color: teal;
+`
+
+const Announcement = () => {
+  return (
+    <Container>
+        Hey now! Hey now, don't dream it's over!
+    </Container>
+  )
+}
+
+export default Announcement
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import Announcements from '../Components/Announcements'
+import Announcement from '../Components/Announcement'
 import Navbar from '../Components/Navbar'
-import styled from 'styled-components'
 
-
-const Container = styled.div`
-    height: 60px;
-    background-color: black;
-`
 const Home = () => {
   return (
-    <Container>
-      <Announcements/>
+    <div>
+      <Announcement/>
       <Navbar/>
-    </Container>
+    </div>
   )
 }
 
