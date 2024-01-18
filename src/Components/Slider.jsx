10a8bc63f6cbf217904eba566c37be63e8547fc4
Raymund Ogsimer <raymund.aicpa@gmail.com>
2024-01-18 15:25:09 +0800

    merge Slider Items

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,12 +1,14 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material"
 import styled from "styled-components"
+import {slideItems} from "../data"
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    background-color: coral;
+    background-color: sky-blue;
     position: relative;
+    overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -24,15 +26,90 @@ const Arrow = styled.div`
     right: ${props=> props.direction === "right" && "10px"};
     margin: auto;
     cursor: pointer;
+    opacity: 0.5;
+    z-index: 2;
 `
 
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    transform: translateX(${props=>props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    background-color; #${props=>props.bg}
+`;
+
+const ImageContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`;
+
+const Image = styled.img`
+    height: 90%;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`;
+const Title = styled.h1`
+    font-size: 70px;
+`;
+
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`;
+
+
+
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0);
+    const handleClick = (direction) => {
+
+        if(direction==="left"){
+            setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2);
+        } else {
+
+            setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0);
+        }
+    };
+
   return (
     <Container>
-       <Arrow direction="left">
+       <Arrow direction="left" onClick={()=>handleClick("left")}>
          <ArrowLeftOutlined/>
        </Arrow>
-       <Arrow direction="right">
+            <Wrapper slideIndex={slideIndex}>
+                {slideItems.map((item) => (
+                <Slide bg={item.bg}>
+                <ImageContainer>
+                    <Image src={item.img} />
+                </ImageContainer>
+                <InfoContainer>
+                    <Title>{item.title}</Title>
+                    <Desc>{item.desc}</Desc>
+                    <Button>PALIT NA</Button>
+                </InfoContainer>
+                </Slide>
+                ))}
+            </Wrapper>
+
+       <Arrow direction="right" onClick={()=>handleClick("right")}>
          <ArrowRightOutlined/>
        </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const slideItems = [
+    {
+        id: 1,
+        img: "https://img.freepik.com/free-photo/high-fashion-portrait-young-beautiful-brunette-woman-wearing-nice-trendy-blue-suit-sexy-fashion-model-posing-studio-fashionable-female-isolated_158538-22180.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1700179200&semt=ais",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE YOUR SEDUCTIVE LOOK! GET 80% DISCOUNTS ON NEW ARRIVALS!",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://img.freepik.com/free-photo/high-fashion-portrait-young-beautiful-brunette-woman-wearing-nice-trendy-blue-suit-sexy-fashion-model-posing-studio-fashionable-female-isolated_158538-22180.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1700179200&semt=ais",
+        title: "CHRISTMAS SALE",
+        desc: "FLAUNT YOUR SEDUCTIVE LOOK THIS CHRISTMAS! GET 80% DISCOUNTS ON NEW ARRIVALS!",
+        bg: "f5fafd",
+    },
+    {
+        id: 3,
+        img: "https://img.freepik.com/free-photo/high-fashion-portrait-young-beautiful-brunette-woman-wearing-nice-trendy-blue-suit-sexy-fashion-model-posing-studio-fashionable-female-isolated_158538-22180.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1700179200&semt=ais",
+        title: "BACK TO SCHOOL SALE",
+        desc: "BE THE CAMPUSE CRUSH! GET 80% DISCOUNTS ON NEW ARRIVALS!",
+        bg: "f5fafd",
+    },
+];
