3c87e2854bd83c49984b9a9e600fd528464be8ee
Raymund Ogsimer <raymund.aicpa@gmail.com>
2024-01-19 23:05:36 +0800

    Initial Commit

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -80,12 +80,10 @@ const Button = styled.button`
 const Slider = () => {
     const [slideIndex, setSlideIndex] = useState(0);
     const handleClick = (direction) => {
-
-        if(direction==="left"){
-            setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2);
+        if(direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
         } else {
-
-            setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0);
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
         }
     };
 
